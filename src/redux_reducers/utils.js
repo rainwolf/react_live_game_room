67301cbf1f97b39766f6cb7b4e488ae5779bcc62
rainwolf <rainwolf@submanifold.be>
2019-03-19 21:59:02 +0100

    util functions

--- src/redux_reducers/utils.js
+++ src/redux_reducers/utils.js
@@ -1,5 +1,62 @@
+import React from 'react';
 
+const server = "https://www.pente.org";
 
+function crownimage(c) {
+    if (c === 0) { return ""; }
+    switch (c) {
+        case 1: return <img alt="crown" src={server + '/gameServer/images/crown.gif'}/>;
+        case 2: return <img alt="crown" src={server + '/gameServer/images/scrown.gif'}/>;
+        case 3: return <img alt="crown" src={server + '/gameServer/images/bcrown.gif'}/>;
+        default: return <img alt="crown" src={server + '/gameServer/images/kothcrown' + (c-3) + '.png'}/>;
+    }
+}
+function userhtml(user) {
+    return (
+        <span className="name">
+            <a href={server + "/gameServer/profile?viewName=" + user.name} target="_blank" rel="noopener noreferrer">
+                { user.subscriber?(
+                    <span style={{fontWeight:'bold', color:'#'+rgb2hex(user.name_color)}}>
+                        {user.name}
+                    </span> ):user.name
+                }
+            </a> &nbsp;
+            {crownimage(user.crown)}
+        </span>
+    );
+}
+function rating(game) {
+    if (game === undefined) return ""; 
+    const r = this.game_ratings[game];
+    let gif = "/gameServer/images/ratings_";
+    if (r >= 1900) {
+        gif += "red.gif";
+    }
+    else if (r >= 1700) {
+        gif += "yellow.gif";
+    }
+    else if (r >= 1400) {
+        gif += "blue.gif";
+    }
+    else if (r >= 1000) {
+        gif += "green.gif";
+    }
+    else {
+        gif += "gray.gif";
+    }
+    return (
+        <div>
+            <img alt="rating" src={server + gif}/>
+            &nbsp; {r}
+        </div>
+    );
+}
+function avatar(subscriber, name) {
+    if (subscriber) {
+        return server+"/gameServer/avatar?name="+name;
+    }
+    return "";
+}
 export function processUser(userdata, state) {
     let user = { 
         name: userdata.player,
@@ -10,6 +67,7 @@ export function processUser(userdata, state) {
     };
     if (user.subscriber) {
         user.name_color = userdata.dsgPlayerData.nameColor.value;
+        // console.log('color: ' + user.name_color.toString(16))
     }
     let crown = 0;
     for(let i = 0; i<userdata.dsgPlayerData.gameData.length; i++) {
@@ -27,5 +85,30 @@ export function processUser(userdata, state) {
         }
     }
     user.crown = crown;
+    user.userhtml = userhtml(user);
+    user.avatar = avatar(user.subscriber, user.name);
+    user.rating = rating;
     state.users = { ...state.users, [user.name]: user };
+}
+function rgb2hex(n) {
+    const b = n & 255, g = (n >> 8) & 255, r = (n >> 16) & 255;
+    const f = s => { const h = s.toString(16); if (h.length < 2) { return '0'+h; } else { return h; }};
+    // console.log(f(r)+f(g)+f(b))
+    return f(r)+f(g)+f(b);
+}
+        
+export function addRoomMessage(data, state) {
+    const messages = state.room_messages.slice();
+    const user = state.users[data.player];
+    messages.push( {
+        message: data.text,
+        player: user
+    });
+    state.room_messages = messages;
+}
+
+export function exitUser(name, state) {
+    let users = { ...state.users }
+    delete users[name];
+    state.users = users;
 }
