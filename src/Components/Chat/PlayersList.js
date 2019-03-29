d6078fde221a53b6eb44e3cc6560bc54ca0e3cf3
rainwolf <rainwolf@submanifold.be>
2019-03-29 15:57:21 +0100

    misc

--- src/Components/Chat/PlayersList.js
+++ src/Components/Chat/PlayersList.js
@@ -20,25 +20,6 @@ const styles = theme => ({
 function PlayersList(props) {
     const { classes, players, game } = props;
     
-    // const listhtml = Object.keys(players).map((name, i) => {
-    //     const player = players[name];
-    //     return (
-    //         <ListItem key={i} >
-    //             <ListItemAvatar>
-    //                 <Avatar alt="Remy Sharp" src={player.avatar} />
-    //             </ListItemAvatar>
-    //             <ListItemText
-    //                 primary={player.userhtml}
-    //                 secondary={player.rating(game)}
-    //             />
-    //         </ListItem>
-    //
-    //     )
-    // });
-    // for (let i=0; i<15; i++) {
-    //     listhtml.push(listhtml[0]);
-    // }
-    
     return (
         <List className={classes.root}>
             {Object.keys(players).map((name, i) => {
