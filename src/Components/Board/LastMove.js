dce98aec15dff9c922a02c74fbb3fef1ce4852a9
rainwolf <rainwolf@submanifold.be>
2019-04-07 22:07:35 +0200

    stupid

--- src/Components/Board/LastMove.js
+++ src/Components/Board/LastMove.js
@@ -3,7 +3,7 @@ import React from 'react';
 function LastMove(props) {
     return (
         <svg key={'last_move'} height={props.size} width={props.size}>
-            <circle cx={props.size/2 - props.size/5} cy={props.size/2 - props.size/5} r={props.size/5}
+            <circle cx={props.size/2} cy={props.size/2} r={props.size/5}
                     fill={"#ff0000"} pointerEvents={'none'} />
         </svg>
     );
