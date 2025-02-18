72b17da432c71e2f8e80dd6480e134e801493395
rainwolf <rainwolf@submanifold.be>
2025-02-18 12:52:58 +0800

    rspack migration

---
+++ rsbuild.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, loadEnv } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+
+const { publicVars, rawPublicVars } = loadEnv({ prefixes: ['REACT_APP_'] });
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  html: {
+    template: './public/index.html',
+  },
+  source: {
+    define: {
+      ...publicVars,
+      'process.env': JSON.stringify(rawPublicVars),
+    },
+  },
+});
+
